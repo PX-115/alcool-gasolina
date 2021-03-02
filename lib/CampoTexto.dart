7a2047c2f62b1bc57e860483e62f67f851b8833d
Vitor Hugo <vitor_hugo_ps@hotmail.com>
2021-03-02 15:09:40 -0300

    0.5.0
    Interface base

---
+++ lib/CampoTexto.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CampoTexto extends StatefulWidget {
+  @override
+  _CampoTextoState createState() => _CampoTextoState();
+}
+
+class _CampoTextoState extends State<CampoTexto> {
+
+  TextEditingController _controllerAlcool = TextEditingController();
+  TextEditingController _controllerGasolina = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.yellow[600],
+        title: Text('Álcool ou gasolina?'),
+      ),
+
+      body: Column(
+        children: <Widget>[
+
+          Padding(
+            padding: EdgeInsets.fromLTRB(32, 32, 32, 10),
+            child: Image.asset('images/logo.png'),
+          ),
+
+          Padding(
+            padding: EdgeInsets.fromLTRB(32, 20, 32, 0),
+            child: Text('Saiba qual a melhor opção para abastecer seu carro',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w600
+              ),
+            )
+          ),
+
+          Padding(
+            padding: EdgeInsets.fromLTRB(32, 10, 32, 10),
+            child: TextField(
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.singleLineFormatter
+              ],
+              decoration: InputDecoration(
+                labelText: 'Preço do álcool, Ex: 1.59',
+                labelStyle: TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w500
+                ),
+                focusColor: Colors.blue
+              ),
+              onSubmitted: (String valorAlcool){
+
+              },
+              controller: _controllerAlcool,
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.fromLTRB(32, 10, 32, 10),
+            child: TextField(
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.singleLineFormatter
+              ],
+              decoration: InputDecoration(
+                labelText: 'Preço da gasolina, Ex: 3.59',
+                labelStyle: TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w500
+                ),
+                focusColor: Colors.blue
+              ),
+              onSubmitted: (String valorGasolina){
+
+              },
+              controller: _controllerGasolina,
+            ),
+          ),
+        
+          SizedBox(
+            height: 50,
+            width: 200,
+            child: RaisedButton(
+              color: Colors.yellow[600],
+              child: Text('Calcular', 
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                )
+              ),
+              onPressed: (){
+                print('Valor digitado: ' + _textEditingController.text);
+              },
+            )
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:entrada_dados/CampoTexto.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: CampoTexto(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
